83556ddffb59b3ff89d0432608388fbfbaca816b
unknown <samhbk2005@gmail.com>
2026-03-12 12:24:37 +0530

    English Baseline Training

---
+++ src/detector/convert_ifind.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv("data/raw/IFND.csv", encoding="latin1")
+
+df = df[["Statement","Label"]]
+
+df.columns = ["text","label"]
+
+df["label"] = df["label"].apply(lambda x: 1 if x == "TRUE" else 0)
+
+df.to_csv("data/processed/indian_news_dataset.csv", index=False)
+
+print(df.head())
+print(df["label"].value_counts())
